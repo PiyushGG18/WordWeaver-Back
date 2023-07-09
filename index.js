5b14f7ee03baa3aa94142b158da41a2c3c0fe388
Piyush <gawadepiyush007@gmail.com>
2023-07-10 01:08:14 +0530

    cors added

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const categoryRoute = require("./routes/categories")
 const multer = require("multer");
 const path = require("path");
 const PORT = process.env.PORT || 5000
+const cors = require("cors");
+
+app.use(cors());
 
 dotenv.config();
 app.use(express.json());
